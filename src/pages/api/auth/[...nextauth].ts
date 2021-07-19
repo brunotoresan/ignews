c4ec7e34b33399afae4c32f1ddb4cfce351ab92b
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-07-19 12:27:59 -0300

    authenticating with Next Auth and Github

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
+      scope: 'read:user'
+    })
+  ]
+})
