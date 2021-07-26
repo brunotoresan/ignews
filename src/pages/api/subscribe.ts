07c11e0134eadefe631f4354e0d89efc6cbbc0ea
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-07-26 18:17:37 -0300

    validating active subscription

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -5,65 +5,65 @@ import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
 
 type User =  {
-    ref: {
-        id: string
-    },
-    data: {
-        stripe_customer_id: string
-    }
+  ref: {
+    id: string
+  },
+  data: {
+    stripe_customer_id: string
+  }
 }
 
 // https://stripe.com/docs/api/checkout/sessions
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'POST') {
-        const session = await getSession({ req })
+  if (req.method === 'POST') {
+    const session = await getSession({ req })
 
-        const user = await fauna.query<User>(
-            query.Get(
-                query.Match(
-                    query.Index('user_by_email'),
-                    query.Casefold(session.user.email)
-                )
-            )
+    const user = await fauna.query<User>(
+      query.Get(
+        query.Match(
+          query.Index('user_by_email'),
+          query.Casefold(session.user.email)
         )
+      )
+    )
 
-        let customerId = user.data.stripe_customer_id
+    let customerId = user.data.stripe_customer_id
 
-        if(!customerId){
-            const stripeCustomer = await stripe.customers.create({
-                email: session.user.email,
-            })
+    if(!customerId){
+      const stripeCustomer = await stripe.customers.create({
+          email: session.user.email,
+      })
 
-            await fauna.query(
-                query.Update(
-                    query.Ref(query.Collection('users'), user.ref.id),
-                    {
-                        data: {
-                            stripe_customer_id: stripeCustomer.id
-                        }
-                    }
-                )
-            )
+      await fauna.query(
+        query.Update(
+          query.Ref(query.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
 
-            customerId = stripeCustomer.id
-        }
-
-        const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: customerId,
-            payment_method_types: ['card'],
-            billing_address_collection: 'required',
-            line_items: [
-                { price: 'price_1JEhaYJdcbSUa6Vw2FBGyyf5', quantity: 1 }
-            ],
-            mode: 'subscription',
-            allow_promotion_codes: true,
-            success_url: process.env.STRIPE_SUCCESS_URL,
-            cancel_url: process.env.STRIPE_CANCEL_URL
-        })
-
-        return res.status(200).json({ sessionId: stripeCheckoutSession.id})
-    } else {
-        res.setHeader('Allow', 'POST')
-        res.status(405).end('Method not allowed')
+      customerId = stripeCustomer.id
     }
+
+  const stripeCheckoutSession = await stripe.checkout.sessions.create({
+    customer: customerId,
+    payment_method_types: ['card'],
+    billing_address_collection: 'required',
+    line_items: [
+        { price: 'price_1JEhaYJdcbSUa6Vw2FBGyyf5', quantity: 1 }
+    ],
+    mode: 'subscription',
+    allow_promotion_codes: true,
+    success_url: process.env.STRIPE_SUCCESS_URL,
+    cancel_url: process.env.STRIPE_CANCEL_URL
+  })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id})
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
 }
