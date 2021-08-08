59e02db1a1c1f85119995493640d0e5c64a79dfd
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-08-08 19:03:32 -0300

    adding explanatory comments

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,5 +1,7 @@
 import Prismic from '@prismicio/client'
 
+// req for server side rendering (only in getServerSideProps)
+// see Prismic docs for more info
 export function getPrismicClient(req?: unknown) {
     const prismic = Prismic.client(
         process.env.PRISMIC_ENDPOINT,
